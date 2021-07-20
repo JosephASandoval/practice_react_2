b284801bdc379576bade1068fc36454448c1befb
JosephASandoval <josephasandoval17@gmail.com>
2021-07-20 14:01:43 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      loading: false,
       character: {},
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     fetch("https://swapi.dev/api/people/1")
       .then((response) => response.json())
       .then((data) => {
         this.setState({
+          loading: false,
           character: data,
         });
       });
   }
 
   render() {
+    const text = this.state.loading ? "loading..." : this.state.character.name;
     return (
       <div>
-        <h1>{this.state.character.name}</h1>
+        <p>{text}</p>
       </div>
     );
   }
