45f58da1aa9b3f063a1fc28cc654991101165aa3
JosephASandoval <josephasandoval17@gmail.com>
2021-07-20 13:51:58 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      character: {},
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.dev/api/people/1")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          character: data,
+        });
+      });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1>{this.state.character.name}</h1>
       </div>
     );
   }
