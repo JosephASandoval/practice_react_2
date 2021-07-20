844b28bff16c6a648a174fefa0ebf7c718dacede
JosephASandoval <josephasandoval17@gmail.com>
2021-07-20 14:45:13 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,30 +3,52 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      loading: false,
-      character: {},
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ loading: true });
-    fetch("https://swapi.dev/api/people/1")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          loading: false,
-          character: data,
-        });
-      });
+    this.state = {};
   }
 
   render() {
-    const text = this.state.loading ? "loading..." : this.state.character.name;
     return (
-      <div>
-        <p>{text}</p>
-      </div>
+      <main>
+        <form>
+          <input placeholder="First Name" />
+
+          <br />
+
+          <input placeholder="Last Name" />
+
+          <br />
+
+          <input placeholder="Age" />
+
+          <br />
+
+          {/* Create radio buttons for gender here */}
+
+          <br />
+
+          {/* Create select box for location here */}
+
+          <br />
+
+          {/* Create check boxes for dietary restrictions here */}
+
+          <br />
+
+          <button>Submit</button>
+        </form>
+
+        <hr />
+
+        <h2>Entered information:</h2>
+        <p>Your name: {/* First and last name here */}</p>
+        <p>Your age: {/* Age here */}</p>
+        <p>Your gender: {/* Gender here */}</p>
+        <p>Your destination: {/* Destination here */}</p>
+        <p>
+          Your dietary restrictions:
+          {/* Dietary restrictions here, comma separated */}
+        </p>
+      </main>
     );
   }
 }
